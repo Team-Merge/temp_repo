7d905a47a5e48e8fa7e7a12b7ffb1af6dffe98fc
Younghyun SEO <tigerseo7686@gmail.com>
2025-01-08 22:39:31 +0900

    feat:test

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,8 +1,9 @@
 module.exports = {
-    extends: ['@commitlint/config-conventional'],
-    rules: {
-      'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'chore']],
-      'header-max-length': [2, 'always', 72],
-    },
-  };
-  
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'chore']],
+    'header-max-length': [2, 'always', 72],
+    'type-empty': [0], // type 비어 있어도 허용
+    'subject-empty': [0], // subject 비어 있어도 허용
+  },
+};
