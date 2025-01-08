843133ec1cda7955a5c0ee7abdda82017cb4aa41
Younghyun SEO <tigerseo7686@gmail.com>
2025-01-08 23:05:56 +0900

    fix:테스트중

--- commitlint.config.js
+++ commitlint.config.js
@@ -3,7 +3,8 @@ module.exports = {
   rules: {
     'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'chore']],
     'header-max-length': [2, 'always', 72],
-    'type-empty': [2, 'never'], // type이 비어 있으면 실패
-    'subject-empty': [2, 'never'], // subject가 비어 있으면 실패
+    'type-empty': [2, 'never'],
+    'subject-empty': [2, 'never'],
+    'subject-case': [0], // 주제(case) 검사를 비활성화 (한글 허용)
   },
 };
