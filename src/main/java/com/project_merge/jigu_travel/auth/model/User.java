dd6bacb2dd0f52682156e1c78f94e071cd86f877
rkdalsrn314 <rkdalsrn314@gmail.com>
2025-01-16 17:14:44 +0900

    fix: modify-user-entity - User 엔티티 수정 및 관련 클래스 수정하였습니다.

--- src/main/java/com/project_merge/jigu_travel/auth/model/User.java
+++ src/main/java/com/project_merge/jigu_travel/auth/model/User.java
@@ -1,7 +1,10 @@
 package com.project_merge.jigu_travel.auth.model;
 
+import com.project_merge.jigu_travel.global.entity.BaseEntity;
 import jakarta.persistence.*;
 import lombok.*;
+
+import java.util.UUID;
 
 @Entity
 @Table(name = "users")
@@ -10,17 +13,35 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class User {
+public class User extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private UUID userId;
 
     @Column(nullable = false)
-    private String username;
+    private String loginId;
 
     @Column(nullable = false)
     private String password;
 
-    private String role;
+    @Column(nullable = false)
+    private String nickname;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Location location;
+
+    @Builder.Default
+    @Column(nullable = false)
+    private Boolean isAdmin = false;
+
+    @Builder.Default
+    @Column(nullable = false)
+    private Boolean deleted = false;
+
+    public enum Location {
+        KOR, USA, JPN
+    }
+
 }
--- src/main/java/com/project_merge/jigu_travel/auth/service/CustomUserDetailsService.java
+++ src/main/java/com/project_merge/jigu_travel/auth/service/CustomUserDetailsService.java
@@ -2,6 +2,7 @@ package com.project_merge.jigu_travel.auth.service;
 
 import com.project_merge.jigu_travel.auth.model.User;
 import com.project_merge.jigu_travel.auth.repository.UserRepository;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.*;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -21,13 +22,13 @@ public class CustomUserDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = userRepository.findByUsername(username)
+        User user = userRepository.findByNickname(username)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found: " + username));
 
         return new org.springframework.security.core.userdetails.User(
-                user.getUsername(),
+                user.getNickname(),
                 user.getPassword(), // 암호화된 비밀번호
-                Collections.singleton(() -> user.getRole())
+                Collections.singleton(new SimpleGrantedAuthority(user.getIsAdmin() ? "ROLE_ADMIN" : "ROLE_USER"))
         );
     }
 }
