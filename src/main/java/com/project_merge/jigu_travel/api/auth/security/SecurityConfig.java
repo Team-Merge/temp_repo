dbfa58777118b9f8ee597e3c3b9ee43253a9e947
Lucky-SeoYounghyun <tigerseo7686@gmail.com>
2025-01-23 13:27:22 +0900

    feat: delete_data

--- src/main/java/com/project_merge/jigu_travel/api/auth/security/SecurityConfig.java
+++ src/main/java/com/project_merge/jigu_travel/api/auth/security/SecurityConfig.java
@@ -67,7 +67,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://127.0.0.1:8080", "http://localhost:8080", "https://jiangxy.github.io"));
+        configuration.setAllowedOrigins(List.of("http://127.0.0.1:8080", "http://localhost:8080", "https://jiangxy.github.io", "http://127.0.0.1:5173", "http://localhost:5173"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setExposedHeaders(List.of(
                 "Authorization",
